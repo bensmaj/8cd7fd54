e30e4e64a4f35a9865090bc8f3e84499f7751e76
bensmaj <ben.stambuk@gmail.com>
2024-07-19 14:15:45 -0400

    fixed the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,50 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
   module: {
     rules: [
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
   plugins: [
-    new HtmlWebPackPlugin({
+    new HtmlWebpackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true, // This is important for client-side routing
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 };
